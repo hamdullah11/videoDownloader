8a28f97c839caabeca55974b0b2873f977e3231f
hamdullah11 <hamdullahlw@gmail.com>
2022-05-26 17:21:15 +0500

    error 1

--- server.js
+++ server.js
@@ -123,7 +123,6 @@ app.get("/download/excel/:json", (req, res) => {
 app.listen(process.env.PORT || 3000, function () {
   console.log(
     "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
+    process.env.PORT
   );
 });
